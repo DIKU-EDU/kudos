41e09b2c67d2cccdf1a2a4db4e7b948cdfa17dbd
Nicklas Warming Jacobsen <nicklaswj@gmail.com>
2016-07-08 13:20:16 +0200

    Added script that creates a bootable iso with kudos and runs qemu

---
+++ run_qemu.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+ISO_PATH=./qemu/kudos.iso 
+KUDOS_PATH=./kudos/kudos-x86_64
+
+if [ -f $ISO_PATH ]; then
+    rm $ISO_PATH
+fi
+
+cp -f $KUDOS_PATH ./qemu/grub/iso/boot/kudos-x86_64
+
+grub-mkrescue -o $ISO_PATH ./qemu/grub/iso
+
+qemu-system-x86_64 -monitor stdio -boot d -m 128 -cdrom  $ISO_PATH -net nic,vlan=0 -net user,vlan=0
