58be579da7e5a21e349850cd3ab3166e407e5cfd
Nicklas Warming Jacobsen <nicklaswj@gmail.com>
2016-07-08 18:34:52 +0200

    Changed the the qemu script to attach the disk on another IDE bus

--- run_qemu.sh
+++ run_qemu.sh
@@ -11,4 +11,4 @@ cp -f $KUDOS_PATH ./qemu/grub/iso/boot/kudos-x86_64
 
 grub-mkrescue -o $ISO_PATH ./qemu/grub/iso
 
-qemu-system-x86_64 -monitor stdio -boot d -m 128 -cdrom  $ISO_PATH -net nic,vlan=0 -net user,vlan=0
+qemu-system-x86_64 -monitor stdio -boot d -m 128 -cdrom  $ISO_PATH -net nic,vlan=0 -net user,vlan=0 -drive format=raw,file='./store.file',bus=1
