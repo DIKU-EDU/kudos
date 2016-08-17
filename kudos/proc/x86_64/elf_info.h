a7ada44047cda0a1481f14ccf464bc6e886601b5
Nicklas Warming Jacobsen <nicklaswj@gmail.com>
2016-08-17 15:04:35 +0200

    Made the elf reader 64 bit compatible

---
+++ kudos/proc/x86_64/elf_info.h
@@ -0,0 +1,14 @@
+
+typedef struct {
+    uint64_t entry_point; /* Entry point for the code */
+
+    uint64_t ro_location; /* Location of RO segment in the file */
+    uint64_t ro_size;     /* Size of RO segment in the file */
+    uint64_t ro_pages;    /* Pages needed by the RO segment */
+    uint64_t ro_vaddr;    /* Virtual address of the RO segment */
+
+    uint64_t rw_location; /* Location of RW segment in the file */
+    uint64_t rw_size;     /* Size of RW segment in the file */
+    uint64_t rw_pages;    /* Pages needed by the RW segment */
+    uint64_t rw_vaddr;    /* Virtual address of the RW segment */
+} elf_info_t;
