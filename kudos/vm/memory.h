a610089b89df7a588383ed800eb932d19e910fee
Nicklas Warming Jacobsen <nicklaswj@gmail.com>
2016-08-18 20:36:53 +0200

    Typo

--- kudos/vm/memory.h
+++ kudos/vm/memory.h
@@ -23,7 +23,7 @@ void physmem_freeblock(void *ptr);
 void physmem_freeblocks(void *ptr, uint32_t size);
 
 /* Virtual Memory Management */
-define USERLAND_STACK_TOP 0xFFFFFFFFFFFFFFFF\
+#define USERLAND_STACK_TOP 0xFFFFFFFFFFFFFFFF\
     -(CONFIG_USERLAND_STACK_SIZE*PAGE_SIZE)
 
 /* For the architecture that supports it */
