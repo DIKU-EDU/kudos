a897851489353a8ccde46961c65ea136a7e1f114
oleks <oleks@oleks.info>
2016-08-29 10:13:09 +0200

    Remove commented out line

--- run_qemu.sh
+++ run_qemu.sh
@@ -11,5 +11,4 @@ cp -f $KUDOS_PATH ./qemu/grub/iso/boot/kudos-x86_64
 
 grub-mkrescue -o $ISO_PATH ./qemu/grub/iso
 
-#qemu-system-x86_64 -monitor stdio -boot d -m 128 -cdrom  $ISO_PATH -net nic,vlan=0 -net user,vlan=0 -drive format=raw,file='./store.file' #,bus=1
 qemu-system-x86_64 -s -monitor stdio -m 128 -net nic,vlan=0 -net user,vlan=0 -drive file=$ISO_PATH,if=ide,bus=0,unit=0,media=cdrom -drive format=raw,if=ide,file='./store.file',bus=0,unit=1
