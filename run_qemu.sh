4850e2600e1bea67b4fc29c2c869535ded5d6845
oleks <oleks@oleks.info>
2016-10-30 00:33:26 +0200

    Use conventional casing in variable names

--- run_qemu.sh
+++ run_qemu.sh
@@ -2,17 +2,17 @@
 
 set -euo pipefail
 
-ISO_PATH=./qemu/kudos.iso
-KUDOS_DISK_PATH=./store.file
-KUDOS_PATH=./kudos/kudos-x86_64
+iso_path=./qemu/kudos.iso
+kudos_disk_path=./store.file
+kudos_path=./kudos/kudos-x86_64
 
-if [ -f "$ISO_PATH" ]; then
-  rm "$ISO_PATH"
+if [ -f "$iso_path" ]; then
+  rm "$iso_path"
 fi
 
-cp -f "$KUDOS_PATH" "./qemu/grub/iso/boot/kudos-x86_64"
+cp -f "$kudos_path" "./qemu/grub/iso/boot/kudos-x86_64"
 
-grub-mkrescue -o "$ISO_PATH" "./qemu/grub/iso"
+grub-mkrescue -o "$iso_path" "./qemu/grub/iso"
 
 qemu-system-x86_64 \
   -gdb tcp::1234    \
@@ -20,5 +20,5 @@ qemu-system-x86_64 \
   -m 128            `# megs of RAM` \
   -net nic          `# emulate a network interface card` \
   -net user         `# enable user mode networking` \
-  -drive file="$ISO_PATH",if=ide,bus=0,unit=0,media=cdrom \
-  -drive file="$KUDOS_DISK_PATH",format=raw,if=ide,bus=0,unit=1
+  -drive file="$iso_path",if=ide,bus=0,unit=0,media=cdrom \
+  -drive file="$kudos_disk_path",format=raw,if=ide,bus=0,unit=1
