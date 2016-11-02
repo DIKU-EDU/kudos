7f918a6a411df39041377be0acadced7ee70947b
Michael Kirkedal Thomsen <kirkedal@acm.org>
2016-11-02 14:51:40 +0100

    run script take an argument that is the program to be run

--- run_qemu.sh
+++ run_qemu.sh
@@ -14,6 +14,18 @@ cp -f "$kudos_path" "./qemu/grub/iso/boot/kudos-x86_64"
 
 grub-mkrescue -o "$iso_path" "./qemu/grub/iso"
 
+rm qemu/grub/iso/boot/grub/grub.cfg
+
+touch qemu/grub/iso/boot/grub/grub.cfg 
+
+echo "set timeout=0
+set default=0 # Set the default menu entry
+ 
+menuentry \"kudos\" {
+   multiboot /boot/kudos-x86_64 initprog=[disk]$1 # The multiboot command replaces the kernel command
+   boot
+}" >> ./qemu/grub/iso/boot/grub/grub.cfg 
+
 qemu-system-x86_64 \
   -gdb tcp::1234    \
   -monitor stdio    `# non graphical mode` \
