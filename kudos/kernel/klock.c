b91a972e9c8889fbb3be6156ad5f5efcbf5793da
oleks <oleks@oleks.info>
2016-11-04 21:04:31 +0100

    An actually working klock

--- kudos/kernel/klock.c
+++ kudos/kernel/klock.c
@@ -4,18 +4,18 @@
 
 void
 klock_init(klock_t *klock) {
-  spinlock_reset(&klock);
+  spinlock_reset(klock);
 }
 
 klock_status_t
 klock_lock(klock_t *klock) {
   interrupt_status_t st = _interrupt_disable();
-  spinlock_acquire(&klock);
+  spinlock_acquire(klock);
   return st;
 }
 
 void
 klock_open(klock_status_t st, klock_t *klock) {
-  spinlock_release(&klock);
+  spinlock_release(klock);
   _interrupt_set_state(st);
 }
